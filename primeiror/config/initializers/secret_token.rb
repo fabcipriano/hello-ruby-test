c8af8a87adb25a8c134c107a011bbc349b8cf85e
Fabiano Cipriano de Oliveira <fabcipriano@yahoo.com.br>
2013-05-02 01:55:51 -0300

    remove projeto Rails

--- primeiror/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Primeiror::Application.config.secret_token = '7ed73db46c7315b55b7a5a687815fe88c259c253faa6ad34cf01652752ccac9c927ef1d5191278d179544af68cacc7a7ca90c24d246b6c161798eb4b3a402c1b'
