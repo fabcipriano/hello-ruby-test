51d27ad748baba6f9edc8d35d472a8aae187f364
Fabiano C. Oliveira <fabianocp@algartelecom.com.br>
2013-05-07 10:11:30 -0300

    script para envio de REFRESH Workgroup para OSM

--- utils/httptest.rb
+++ utils/httptest.rb
@@ -34,11 +34,26 @@ puts("Calling http urls...")
 #puts response.body, response.code, response.message, response.headers.inspect
 #pp Page.get('http://www.google.com')
 
-MyTimer.new.repeat_every(5) do
+MyTimer.new.repeat_every(60) do
       puts("========================================== Requisicao HTTP ==========================================")
       puts Time.now.strftime("%T,%L")
-      response = RestClient.get('http://www.google.com.br')
-      puts response.code, response.description, response.headers
+	  puts "----------------- request LOGIN"
+      response = RestClient.get('http://osmhom.network.ctbc:7003/oms/XMLAPI/login?username=oss&password=ctbc2012')
+	  sessionid = response.cookies["JSESSIONID"]
+
+      puts response.code
+	  puts "session_id[#{sessionid}]"
+	  pp response.body
+
+	  puts "----------------- request REFRESH"
+      response = RestClient.get('http://osmhom.network.ctbc:7003/oms/XMLAPI/worklist?xmlDocAdmin=OMSAdminSignal.Request&signal=RefreshServer&refreshItems=Workgroup', {:cookies => {:JSESSIONID => sessionid}})
+      puts response.code
+	  pp response.body
+
+	  puts "----------------- request LOGOUT"
+      response = RestClient.get('http://osmhom.network.ctbc:7003/oms/XMLAPI/logout')
+      puts response.code
+	  pp response.body
     end
 
 
